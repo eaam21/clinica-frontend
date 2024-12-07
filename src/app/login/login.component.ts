5fc0efce09806319e5a33814a1430142246d1ea5
Elias Alejandro <elias.alejandro21@gmail.com>
2024-12-07 10:15:07 -0500

    agregando servicio login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -21,5 +22,19 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './login.component.css'
 })
 export default class LoginComponent {
+  credenciales = { usuario: '', clave: '' };
+  private loginService = inject(LoginService);
 
+  onSubmit(){
+    console.log("TEST");
+    this.loginService.login(this.credenciales).subscribe({
+      next:(response)=>{
+        console.log(response)
+        console.log('Login successful', response);
+      },
+      error:(err)=>{
+        console.error('Login failed', err);
+      }
+    })
+  }
 }
