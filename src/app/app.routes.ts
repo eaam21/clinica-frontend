bde92468a6686c9fd0f1ca5f4e3ac41851c94f82
Elias Alejandro <elias.alejandro21@gmail.com>
2024-12-07 09:46:48 -0500

    Agregando componente login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path:'', loadComponent:()=>import('./inicio/inicio.component')},
+    {path:'', loadComponent:()=>import('./login/login.component')},
+    {path:'inicio', loadComponent:()=>import('./inicio/inicio.component')},
     {path:'registrar', loadComponent:()=>import('./formulario-paciente/formulario-paciente.component')},
     {path:'editar/:id', loadComponent:()=>import('./formulario-paciente/formulario-paciente.component')},
 ];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export default class LoginComponent {
+
+}
