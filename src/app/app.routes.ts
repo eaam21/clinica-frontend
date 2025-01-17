d4e1f16bf4446e94ca199e3464b39730a192b84a
Elias Alejandro <elias.alejandro21@gmail.com>
2025-01-16 22:43:55 -0500

    Guards: restriccion de accesos a urls

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path:'', loadComponent:()=>import('./login/login.component')},
     {path:'salir', loadComponent:()=>import('./logout/logout.component')},
     {path:'inicio', loadComponent:()=>import('./inicio/inicio.component')},
-    {path:'registrar', loadComponent:()=>import('./formulario-paciente/formulario-paciente.component')},
-    {path:'editar/:id', loadComponent:()=>import('./formulario-paciente/formulario-paciente.component')},
+    {path:'registrar', loadComponent:()=>import('./formulario-paciente/formulario-paciente.component'), canActivate:[authGuard]},
+    {path:'editar/:id', loadComponent:()=>import('./formulario-paciente/formulario-paciente.component'), canActivate:[authGuard]},
 ];
 
 @NgModule({
@@ -14,4 +15,3 @@ export const routes: Routes = [
     exports: [RouterModule]
   })
   export class AppRoutingModule { }
-  
